08caad906f6e0bad25d9af0e763c223a9c04d368
Vincent <vincecreton@hotmail.fr>
2020-02-05 09:59:04 +0100

    Timer.js terminé

--- button/src/App.js
+++ button/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/button/Button.js'
 
-
 function App() {
   return (
     <div className="App">
--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -4,15 +4,15 @@ class Timer extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+    this.state = { counter: 0 };
   }
 
   componentDidMount() {
-    // ...
+    this.interval = setInterval(() => this.setState(state => ({counter: state.counter + 1})) , 1000);
   }
 
   componentWillUnmount() {
-    // ...
+    clearInterval(this.interval);
   }
   
   render () {
