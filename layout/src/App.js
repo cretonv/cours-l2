43b6a3595e1439475bbe583e2bfbe7758f06041b
Vincent <vincecreton@hotmail.fr>
2020-02-05 11:22:47 +0100

    Avancement des cards bootstrap

--- layout/src/App.js
+++ layout/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './user.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container-fluid card-color">
+                <div className="row">
+                    <div className="col-1"> </div>
+                    <div className="col-5">
+                        <div className="card mt-2 mb-2">
+                            <img className="card-img-top" src={data.results[0].picture.large} alt="Card image cap"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{data.results[0].name.first} {data.results[0].name.last}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">{data.results[0].location.city}</h6>
+                                <p className="card-text text-left"> email: {data.results[0].email}</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
